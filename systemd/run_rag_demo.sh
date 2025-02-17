1cdb74786d47040066583a39d507e5285efa0a4f
Markus Mielimonka <markus.mielimonka@ibm.com>
2025-02-17 15:47:49 +0100

    feat(systemd): Added a unit file template to automatically start the demo/gardio script for usage in a dedicated demo system

---
+++ systemd/run_rag_demo.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+RAG_MODEL_PATH={{ working_directory }}/model/{{ model_name }} micromamba run -n rag-demo -r %{ conda_dir }% python %{ working_directory }%/repo/run_model.py
