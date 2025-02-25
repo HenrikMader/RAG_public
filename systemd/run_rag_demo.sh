37fa3f03ba79fd2d25b9441ab8d3b2a880673bd9
Markus Mielimonka <markus.mielimonka@ibm.com>
2025-02-25 14:25:36 +0100

    fix(systemd): added user and micromamba resolution to systemd service so that all executable ans paths are resolved correctly

--- systemd/run_rag_demo.sh
+++ systemd/run_rag_demo.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-cd %{ working_directory }%
-RAG_MODEL_PATH=model/%{ model_name }% %{ micromamba_location }% run -n rag-demo -r %{ conda_dir }% python repo/run_model.py
+# NOTE: the relative path is important for run_model to find the DB (in its current state.)
+cd %{ working_directory }%/repo
+RAG_MODEL_PATH=%{ working_directory }%/model/%{ model_name }% %{ micromamba_location }% run -n rag-demo -r %{ conda_dir }% python run_model.py
